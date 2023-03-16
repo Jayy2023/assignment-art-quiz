1c2787ec33a9a0c5b57250af7ed73872d181596f
Jayy2023 <119895456+Jayy2023@users.noreply.github.com>
2023-03-16 14:51:54 -0400

    trying to append images

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-// JavaSript code here
+document.addEventListener('DOMContentLoaded', () => {
+    let btn = document.querySelector('button');
+    let output = document.getElementById('output');
+    btn.onclick = handleClick;
+
+
+    async function handleClick(){
+        const url =  await fetch('https://api.artic.edu/api/v1/artworks?limit=40')
+        .then(response => response.json())
+        .then(data => data)
+        const image = document.createElement('img');
+        image.src = url;
+        output.append('image');
+        
+     console.log(image);
+    }
+
+})
+
+
